28936790510010c6bee163e305c7df1e42b20eb5
eddwardmessias <eddwardmessias@hotmail.com>
2023-01-05 19:33:05 -0300

    Enviando codigo parte 1 - 05/01/23

---
+++ js/literais.js
@@ -0,0 +1,10 @@
+let nome = "Eddward Messias"
+let sobre = "Desenvolvedor Java"
+
+let frase = "Olá meu nome é " + nome + " e eu sou " + sobre
+console.log(frase)
+
+//interpolação
+let texto = `iae meu nobre ${nome} eu sou ${sobre}`
+//posso quebrar linhas tbm
+console.log(texto)
